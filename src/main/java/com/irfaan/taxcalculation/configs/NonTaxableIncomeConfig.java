9951e59228dea8d2f8e5b0ff81e0b8b5ec19b1cd
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2023-01-05 23:34:46 +0700

    add non taxable income enum and config

---
+++ src/main/java/com/irfaan/taxcalculation/configs/NonTaxableIncomeConfig.java
@@ -0,0 +1,32 @@
+/**
+ * DANA
+ * Copyright (c) 2018‐2023 All Rights Reserved.
+ */
+package com.irfaan.taxcalculation.configs;
+
+import com.irfaan.taxcalculation.enums.NonTaxableIncomeEnum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author irfaanhibatullah
+ * @version $Id: NonTaxableIncomeConfig.java, v 0.1 2023‐01‐05 23.25 irfaanhibatullah Exp $$
+ */
+public class NonTaxableIncomeConfig {
+
+    private static final Map<NonTaxableIncomeEnum, Long> configsOfNonTaxableIncomeConfig = new HashMap<>();
+
+    static {
+        configsOfNonTaxableIncomeConfig.put(NonTaxableIncomeEnum.TK_O, 54_000_000L);
+        configsOfNonTaxableIncomeConfig.put(NonTaxableIncomeEnum.K_0, 58_500_000L);
+        configsOfNonTaxableIncomeConfig.put(NonTaxableIncomeEnum.K_1, 63_000_000L);
+        configsOfNonTaxableIncomeConfig.put(NonTaxableIncomeEnum.K_2, 67_000_000L);
+        configsOfNonTaxableIncomeConfig.put(NonTaxableIncomeEnum.K_3, 72_000_000L);
+    }
+
+    public long getAmountOfNonTaxableIncome(NonTaxableIncomeEnum nonTaxableIncomeEnum) {
+        return configsOfNonTaxableIncomeConfig.getOrDefault(nonTaxableIncomeEnum, 0L);
+    }
+
+}
---
+++ src/main/java/com/irfaan/taxcalculation/enums/NonTaxableIncomeEnum.java
@@ -0,0 +1,31 @@
+/**
+ * DANA
+ * Copyright (c) 2018‐2023 All Rights Reserved.
+ */
+package com.irfaan.taxcalculation.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author irfaanhibatullah
+ * @version $Id: NonTaxableIncomeEnum.java, v 0.1 2023‐01‐05 23.26 irfaanhibatullah Exp $$
+ */
+@AllArgsConstructor
+@Getter
+public enum NonTaxableIncomeEnum {
+
+    TK_O("TK_0", "non married"),
+
+    K_0("K_0", "married with 0 children"),
+
+    K_1("K_1", "married with 1 childen"),
+
+    K_2("K_2", "married with 2 childen"),
+
+    K_3("K_3", "married with 3 childen"),
+    ;
+    private final String code;
+
+    private final String description;
+}
