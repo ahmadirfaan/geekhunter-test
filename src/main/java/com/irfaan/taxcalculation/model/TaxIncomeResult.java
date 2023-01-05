0c9d0d5c5d1170d1f8d00f70d3dc8d44b3be8004
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2023-01-05 23:23:00 +0700

    add tax income result

---
+++ src/main/java/com/irfaan/taxcalculation/model/TaxIncomeResult.java
@@ -0,0 +1,18 @@
+package com.irfaan.taxcalculation.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author irfaanhibatullah
+ * @version $Id: TaxIncomeResult.java, v 0.1 2023‐01‐05 23.01 irfaanhibatullah Exp $$
+ */
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class TaxIncomeResult extends BaseResult {
+
+    private long taxIncome;
+
+    private String codeCurrency;
+}
