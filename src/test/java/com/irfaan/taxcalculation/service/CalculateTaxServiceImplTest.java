3e5497d8d8430d2918ddb603f65437b95d1f4aad
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2023-01-06 17:41:28 +0700

    add unit test for indonesian calculation tax

--- src/test/java/com/irfaan/taxcalculation/service/CalculateTaxServiceImplTest.java
+++ src/test/java/com/irfaan/taxcalculation/service/CalculateTaxServiceImplTest.java
@@ -5,21 +5,30 @@ import com.irfaan.taxcalculation.model.TaxIncomeRequest;
 import com.irfaan.taxcalculation.model.TaxIncomeResult;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.HashMap;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class CalculateTaxServiceImplTest {
 
 
-    @Mock
+    @Autowired
     private CalculateTaxService calculateTaxService;
 
     @Test
-    public void testCalculationTaxIndonesian() {
+    void testCalculationTaxIndonesian() {
         TaxIncomeRequest request = new TaxIncomeRequest();
         request.setGender("MALE");
         request.setNationality("IDR");
         request.setMaritalStatus("TK_0");
         request.setBasicSalary("10500000");
+        request.setDeductibleIncome(new HashMap<>());
+        request.setNonDeductibleIncome(new HashMap<>());
 
         //if tax income from value then must be
         double taxResult = 5_800_000.00 / 12;
